39357d758d97c57d6880921c2d3ecfe8793d280e
SkapiN <skapinthefourb@gmail.com>
2015-02-04 19:48:43 +0100

    Update pre-push.sh
    delete stash

--- pre-push.sh
+++ pre-push.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-#We want to run test on commited source, and not current source.
-#So we stash them
-git stash -q --keep-index
 
 echo " > Runing Tests..."
 ./run_tests.sh
 echo " > Finished !"
 RESULT=$?
-
-#Set previous stash source.
-git stash pop -q
 
 [ $RESULT -ne 0 ] && exit 1
 exit 0
