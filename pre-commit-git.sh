5f97d2700a152d806daa079577e133814389ce13
PolyLab-1 <polybox3d@gmail.com>
2015-02-04 18:22:26 +0100

    Add pre-commit

--- pre-commit-git.sh
+++ pre-commit-git.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#We want to run test on commited source, and not current source.
+#So we stash them
+git stash -q --keep-index
 
+./run_tests.sh
+RESULT=$?
 
+#Set previous stash source.
+git stash pop -q
+
+[ $RESULT -ne 0 ] && exit 1
 exit 0
---
+++ run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ./src/
+/usr/lib/i386-linux-gnu/qt5/bin/qmake
+
+cd ../build/
+make
