b458fb77849aed7976e26b140af48ec23333563b
PolyLab-1 <polybox3d@gmail.com>
2015-02-04 18:43:18 +0100

    test

--- pre-commit-git.sh
+++ pre-commit-git.sh
@@ -1,5 +1,12 @@
 #We want to run test on commited source, and not current source.
 #So we stash them
+if [ `date +%w` -eq 3 ]; then
+  echo "Enjoy your life. Do not work on Sunday!"
+  exit 1
+fi
+exit 0
+
+
 git stash -q --keep-index
 
 ./run_tests.sh
